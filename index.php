76765ca5639b2414f4eaa08c3c5f9446a24dca70
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-12-05 19:40:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+
+  <!-- Main style -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Axios -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.0/axios.min.js' integrity='sha512-OdkysyYNjK4CZHgB+dkw9xQp66hZ9TLqmS2vXaBrftfyJeduVhyy1cOfoxiKdi4/bfgpco6REu6Rb+V2oVIRWg==' crossorigin='anonymous'></script>
+
+  <!-- Vue Js -->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <!-- Main js -->
+  <script defer src="js/main.js"></script>
+
+  <title>PHP Dischi JSON</title>
+
+</head>
+<body>
+
+  <div id="app">
+
+  </div>
+  
+</body>
+</html>
